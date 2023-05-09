94582bda45df9e8d6fde4a82dcd32b1ba7e79936
SevenAJJY <yassine.elhajjy@gmail.com>
2023-05-09 16:55:45 +0000

    create main structure of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/portfolio" element={<Portfolio />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
